71991220aa8ddc288ce9c629328d1115c8945296
karimould <karim_om@me.com>
2020-08-02 22:23:14 +0200

    removed tsx extensions from webpack config since we are not using any jsx or tsx

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const CopyPlugin = require("copy-webpack-plugin");
 module.exports = {
   entry: "./js/index.ts",
   resolve: {
-    extensions: [".tsx", ".ts", ".js"],
+    extensions: [".ts", ".js"],
   },
   output: {
     path: path.resolve(__dirname, "dist"),
